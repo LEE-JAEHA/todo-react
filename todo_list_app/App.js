e73b0b71857403d3c0aa061fdcf6166c5cb08b37
jaeha <years6666@gmail.com>
2019-02-18 16:47:12 +0900

    todo design

--- todo_list_app/App.js
+++ todo_list_app/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , 
+  StatusBar, TextInput, Dimensions,
+  Platform,
+} from 'react-native';
+const  {height,width} =Dimensions.get("window")
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your!</Text>
+        <StatusBar barStyle="light-content"/>
+        <Text style={styles.title}>오늘의 할 일!!!</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={"새로 할 일"} />
+        </View>
       </View>
     );
   }
@@ -14,8 +22,34 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1256F3',
     alignItems: 'center',
-    justifyContent: 'center',
+    
+  },
+  title:{
+    color : "white",
+    fontSize: 30,
+    marginTop : 50,
+    marginBottom : 30,
+    fontWeight : "200"
+  },
+  card: {
+    backgroundColor :"white",
+    flex  :1,
+    width : width -30 ,
+    borderTopLeftRadius : 10,
+    borderTopRightRadius : 10,
+    elevation: 5,
+    ...Platform.select({
+      ios:{
+      shadowColor:"rgb(50,50,50)",
+      shadowOpacity:0.5,
+      shadowRadius: 5,
+      shadowOffset : {
+        height:-1,
+        width:0
+      }
+    }
+  })
   },
 });
